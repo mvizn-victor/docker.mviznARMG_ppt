e6338d6df58f4f411f36f9273e777620bb648603
victor <victor@mvizn.com>
2025-04-29 11:24:29 +0800

    startsim endsim

---
+++ PPTARMG_utils/endsim.sh
@@ -0,0 +1,2 @@
+rm /dev/shm/simARMG2
+bash ~/PPTARMG_utils/runall.sh
--- PPTARMG_utils/startsim.sh
+++ PPTARMG_utils/startsim.sh
@@ -1,3 +1,18 @@
 bash ~/PPTARMG_utils/runall.sh
 touch /dev/shm/simARMG2
+echo start simtcds
 bash ~/PPTARMG_utils/simtcds.sh
+echo wait 40 seconds
+for i in {1..40};do
+	echo $i
+	sleep 1
+done
+echo start simclps
+bash ~/PPTARMG_utils/simclps.sh
+echo wait 40 seconds
+for i in {1..40};do
+	echo $i
+	sleep 1
+done
+echo endsim
+bash ~/PPTARMG_utils/endsim.sh
