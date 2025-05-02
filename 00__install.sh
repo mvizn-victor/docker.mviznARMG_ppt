90ef8ffe1026e53b70b5bf58a179593e9e386c0c
Victor <changcw83@gmail.com>
2025-05-02 17:24:13 +0800

    00__install warn when total space of disk hosting /opt below 1.5TB

--- 00__install.sh
+++ 00__install.sh
@@ -5,11 +5,11 @@ echo "checking"
 # Get the total size in kilobytes (1K-blocks) from df
 total_kb=$(df --output=size /opt| tail -1)
 
-# Convert 2TB to kilobytes (2 * 1024 * 1024 * 1024)
-threshold_kb=$((2 * 1024 * 1024 * 1024))
+# Convert 1.5TB to kilobytes (1.5 * 1024 * 1024 * 1024)
+threshold_kb=$((1.5 * 1024 * 1024 * 1024))
 
 if [ "$total_kb" -lt "$threshold_kb" ]; then
-    echo "Total space of /opt is less than 2TB, raid probably not mounted! exiting"
+    echo "Total space of /opt is less than 1.5TB, raid probably not mounted!"
     read -p "Do you want to proceed? Type 'yes' to continue: " user_input
     if [ "$user_input" != "yes" ]; then
         echo "Exiting."
